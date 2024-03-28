31f686f51cbe373fa42428dc7fb7f28ca9ecbdd7
veelcheck <aga_wilczek@onet.eu>
2024-03-28 21:15:48 +0100

    redirect rule added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import Hamburger from './Hamburger';
 
 // const customClassName = ({ isActive, customStyles }) =>
@@ -31,29 +31,29 @@ function Header() {
       role='navigation'
       className='text-2xl py-3 font-amatic sticky md:static top-0 left-0 lg:h-40 md:text-4xl lg:text-6xl md:flex md:justify-between md:items-center md:px-2 border-b z-10 bg-bg-colour'>
       <div className='pl-3'>
-        <NavLink
+        <Link
           to='/'
           aria-label='Home'
           className='onHover text-4xl lg:text-6xl'>
           HI!
-        </NavLink>
+        </Link>
       </div>
       <div
         aria-hidden={!isOpen}
         className={`${isOpen ? 'flex flex-col items-center gap-4' : 'hidden md:flex md:gap-4'}`}>
         <NavLink
           to='/portfolio'
-          className='onActive onHover'>
+          className='onFocus onHover'>
           Portfolio
         </NavLink>
         <NavLink
           to='/skills'
-          className='onActive onHover'>
+          className='onFocus onHover'>
           Skills
         </NavLink>
         <NavLink
           to='/contact'
-          className='onActive onHover'>
+          className='onFocus onHover'>
           Contact
         </NavLink>
       </div>

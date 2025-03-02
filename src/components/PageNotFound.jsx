60367af8478ea14f9f10512ef64cd2051b8686f9
veelcheck <veelcheck@gmail.com>
2025-03-02 11:48:06 +0100

    portfolio update

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -2,10 +2,10 @@ import BackButton from './BackButton';
 
 function PageNotFound() {
   return (
-    <div className='flex flex-col gap-2 items-center pt-8'>
+    <section className='flex flex-col gap-2 items-center pt-8'>
       <h1>Ops, we are getting 404! Page not found. Where is it then?</h1>
       <BackButton />
-    </div>
+    </section>
   );
 }
 
